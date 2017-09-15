9b726bc327ca2b2ba8e4711d000153fe49ecb1e8
famf0002 <famf0002@hotmail.com>
2017-09-15 18:12:41 +0200

    Botones formulario Usuario Modificado

---
+++ webService/grabarRFID.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# importamos el modulo socket
+from __future__ import unicode_literals
+import socket
+import json
+from StringIO import StringIO
+from webService.models import Usuario
+
+
+
+class grabarRFID:
+    def grabar():
+        print 'hola'
